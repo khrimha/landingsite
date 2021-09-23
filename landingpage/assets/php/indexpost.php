87ce2f765d7e73a0062b0e5a49018f6c20c7711d
Emmanuel Jedidiah Sokpoli <90480169+khrimha@users.noreply.github.com>
2021-09-23 08:02:33 +0000

    Add files via upload

---
+++ landingpage/assets/php/indexpost.php
@@ -0,0 +1,11 @@
+<html>  
+   <body>  
+     
+      Welcome <?php echo $_POST["Username"]; ?> </br>  
+      Your new username is: <?php echo $_POST["Username"]; ?> </br>
+      Your password is: <?php echo $_POST["password"]; ?> </br> 
+      Keep it safe from others. </br>
+      Have a nice surf!!!
+  
+   </body>  
+</html>  
